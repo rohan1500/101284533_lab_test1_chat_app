c8829fda75611d6b10db8eac7999c7e58fa04e47
rohan1500 <78766526+rohan1500@users.noreply.github.com>
2022-02-10 00:00:40 -0500

    Add files via upload

--- public/users.js
+++ public/users.js
@@ -29,21 +29,3 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.post("init", (doc) => {
-  console.log("%s has been initialized from the db", doc._id);
-});
-
-UserSchema.post("validate", (doc) => {
-  console.log("%s has been validated (but not saved yet)", doc._id);
-});
-
-UserSchema.post("save", (doc) => {
-  console.log("%s has been saved", doc._id);
-});
-
-UserSchema.post("remove", (doc) => {
-  console.log("%s has been removed", doc._id);
-});
-
-const User = mongoose.model("User", UserSchema);
-module.exports = User;
